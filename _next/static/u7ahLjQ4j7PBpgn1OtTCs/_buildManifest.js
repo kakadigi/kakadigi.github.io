9c5678d6610c0847d0a35af4383e93346bd586a7
ifirmawan <firmawaneiwan@gmail.com>
2021-10-01 08:11:24 +0000

    Deploying to gh-pages from  @ f38cfc8a70a49ab364bac2d6355083ce8d8b6b35 🚀

--- _next/static/u7ahLjQ4j7PBpgn1OtTCs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-24bd61aa97127a8ea271.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
