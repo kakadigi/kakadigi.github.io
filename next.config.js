1b3dd627b5ca9a5292ddc27cfbc0ce0eeb09cc1d
Iwan Firmawan <firmawaneiwan@gmail.com>
2022-04-08 15:39:11 +0700

    chore: Replace github action workflow
    chore: adding export command
    
    fix: deploy token key
    
    fix: deployment action key
    
    its should work
    
    its should work
    
    ignore _next directory at project

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-  /**
-   * Tell Next.js where the `public` folder is.
-   * Replace `nextjs-github-pages` with your Github repo project name.
-   */
-  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
 };
