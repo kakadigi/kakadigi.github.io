bb2cb915aa75b364f68e1aa767741d6e1d4dd336
Iwan Firmawan <hellofirmawan@gmail.com>
2023-09-03 16:47:27 +0700

    chore: revert unoptimized next config set true

--- components/NextImage.jsx
+++ components/NextImage.jsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
+import { imageLoader } from "./loader";
 
 const NextImage = ({ src, alt, width, height }) => {
-  return <Image src={src} alt={alt} width={width} height={height} />;
+  return (
+    <Image
+      loader={imageLoader}
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      unoptimized
+    />
+  );
 };
 
 export default NextImage;
